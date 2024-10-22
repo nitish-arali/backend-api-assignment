83e0d666bfd6904f2f580f180b4cc8bb09b27786
nitish arali <nitisharali1995@gmail.com>
2024-10-22 06:54:26 +0530

    added mongodb atlas uri

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,12 +9,12 @@ import auth from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 // POST /api/agency-clients - Create Agency and Clients
-router.post("/agency-clients", auth, createAgencyAndClients);
+router.post("/agency-clients",auth,  createAgencyAndClients);
 
 // PUT /api/clients/:clientId - Update Client
 router.put("/clients/:clientId", auth, updateClient);
 
 // GET /api/top-client - Get Agency and Top Client
-router.get("/top-client", auth, getTopClient);
+router.get("/top-client",  getTopClient);
 
 export default router;
